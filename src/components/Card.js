d0b8b1514ad7e736ab9a95220bd0709894de9b33
LPlox <lina.js.wang@gmail.com>
2020-04-24 21:51:33 +0200

    Do an npm install before using the commit. Styling for the cards works, fetching icon link not so much.. But this is the furthest I've come

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/card.scss";
 
 const Card = ({ item }) => {
   const { NameOfInitiative, Summary, WebAddress } = item;
+  const [image, setImage] = useState();
+  const apiLogo = "//logo.clearbit.com/";
+
+  useEffect(() => {
+    fetch(apiLogo + WebAddress).then((response) => {
+      if (response.ok) {
+        setImage(true);
+      } else {
+        setImage(false);
+      }
+    });
+  }, [WebAddress]);
+
   return (
     <div className="card">
       <h2>{NameOfInitiative}</h2>
-      <img src={require("../assets/adaptation_fund.png")} alt="logo" />
+      {WebAddress ? (
+        <img src={`${apiLogo + WebAddress}`} alt="logo" />
+      ) : (
+        <img src={require("../assets/IconPlaceholder.png")} alt="placeholder" />
+      )}
+
       <p>{Summary}</p>
       <div className="card-links">
         <button className="tag">Global</button>
-        <a className="homebutton" href={WebAddress}>
-          <object
-            data={require("../assets/homebtn.svg")}
-            aria-label="homebutton"
-          ></object>
-        </a>
+        {WebAddress ? (
+          <a className="homebutton" href={WebAddress}>
+            <object
+              data={require("../assets/homebtn.svg")}
+              aria-label="homebutton"
+            ></object>
+          </a>
+        ) : null}
       </div>
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,46 +1,19 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import Card from "./Card";
+import Masonry from "react-masonry-css";
 import "./styles/cards.scss";
 
 const Cards = ({ data }) => {
-  const inputEl = useRef(null);
-  const [dimensions, setDimensions] = useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
-
-  // useEffect(() => {
-  //   // resizeAllGridItems();
-  //   const allItems = document.getElementsByClassName("item");
-  //   for (let x = 0; x < allItems.length; x++) {
-  //     resizeGridItem(allItems[x]);
-  //   }
-  // }, [dimensions, setDimensions]);
-
-  // function resizeGridItem(item) {
-  //   const grid = document.getElementsByClassName("masonry")[0];
-  //   const rowHeight = parseInt(
-  //     window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
-  //   );
-  //   const rowGap = parseInt(
-  //     window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
-  //   );
-  //   const rowSpan = Math.ceil(
-  //     (inputEl.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap)
-  //   );
-  //   item.style.gridRowEnd = "span " + rowSpan;
-  // }
-
-  console.log(inputEl.getBoundingClientRect().height);
-
   return (
-    <div ref={inputEl} className="masonry">
-      <div className="item content">
-        {data.map((item, i) => {
-          return <Card key={i} item={item} />;
-        })}
-      </div>
-    </div>
+    <Masonry
+      breakpointCols={4}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {data.map((item, i) => {
+        return <Card key={i} item={item} />;
+      })}
+    </Masonry>
   );
 };
 
