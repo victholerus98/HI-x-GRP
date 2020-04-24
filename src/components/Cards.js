792c62527effbd561252743721e9d8fcab4dc314
LPlox <lina.js.wang@gmail.com>
2020-04-24 16:17:33 +0200

    changes for a not working css layout for cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "./Card";
 import "./styles/cards.scss";
 
 const Cards = ({ data }) => {
+  const inputEl = useRef(null);
   const [dimensions, setDimensions] = useState({
     height: window.innerHeight,
     width: window.innerWidth,
   });
 
-  useEffect(() => {
-    resizeAllGridItems();
-  }, [dimensions, setDimensions]);
+  // useEffect(() => {
+  //   // resizeAllGridItems();
+  //   const allItems = document.getElementsByClassName("item");
+  //   for (let x = 0; x < allItems.length; x++) {
+  //     resizeGridItem(allItems[x]);
+  //   }
+  // }, [dimensions, setDimensions]);
+
+  // function resizeGridItem(item) {
+  //   const grid = document.getElementsByClassName("masonry")[0];
+  //   const rowHeight = parseInt(
+  //     window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
+  //   );
+  //   const rowGap = parseInt(
+  //     window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
+  //   );
+  //   const rowSpan = Math.ceil(
+  //     (inputEl.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap)
+  //   );
+  //   item.style.gridRowEnd = "span " + rowSpan;
+  // }
+
+  console.log(inputEl.getBoundingClientRect().height);
 
   return (
-    <div className="masonry">
+    <div ref={inputEl} className="masonry">
       <div className="item content">
         {data.map((item, i) => {
           return <Card key={i} item={item} />;
@@ -22,27 +43,5 @@ const Cards = ({ data }) => {
     </div>
   );
 };
-
-function resizeGridItem(item) {
-  const grid = document.getElementsByClassName("masonry")[0];
-  const rowHeight = parseInt(
-    window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
-  );
-  const rowGap = parseInt(
-    window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
-  );
-  const rowSpan = Math.ceil(
-    (item.querySelector(".content").getBoundingClientRect().height + rowGap) /
-      (rowHeight + rowGap)
-  );
-  item.style.gridRowEnd = "span " + rowSpan;
-}
-
-function resizeAllGridItems() {
-  const allItems = document.getElementsByClassName("item");
-  for (let x = 0; x < allItems.length; x++) {
-    resizeGridItem(allItems[x]);
-  }
-}
 
 export default Cards;
