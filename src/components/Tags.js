f7ae1117db94c665523d8decfe64efa8a617cd24
victholerus98 <55544652+victholerus98@users.noreply.github.com>
2020-04-28 10:34:57 +0200

    made an array in tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -10,9 +10,21 @@ const Tags = ({item}) => {
     FinanceAndBudgets,
     PracticeAndInnovation
   } = item
+  const tagArray = [
+    LinkedToUNCAS,
+    PolicyAndPlanning,
+    KnowledgeAndlearning,
+    FinanceAndBudgets,
+    PracticeAndInnovation
+  ]
+  console.log(tagArray) //prettier-ignore
+  // const [, , , , , , , uncas, , , policy, knowledge, finance, practice] = Object.values(item)
+  // console.log(uncas, policy, knowledge, finance, practice)
   return (
     <div>
-      <button className="tags">{PublicPrivateOrCSOFocus}</button>
+      {PublicPrivateOrCSOFocus && (
+        <button className="tag">{PublicPrivateOrCSOFocus}</button>
+      )}
     </div>
   )
 }
