f85ec7d7a9013025f44c334f53064e4e8074312e
victholerus98 <55544652+victholerus98@users.noreply.github.com>
2020-04-29 16:16:30 +0200

    added dropdown to type

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,12 +1,32 @@
-import React from "react";
-import "./styles/type.scss";
+import React, {useState} from "react"
+import "./styles/type.scss"
+
+const typeItems = [
+  "Fund and financing facilities",
+  "Networks, alliances & partnership",
+  "Organisations",
+  "Programmes & projects"
+]
 
 const Type = () => {
+  const [typeToggle, setTypeToggle] = useState(false)
+  const handleBtnClick = () => setTypeToggle(!typeToggle)
+
   return (
     <div>
-     
+      <div className="typeContainer">
+        <p>Type</p>
+        <button onClick={handleBtnClick}>All</button>
+      </div>
+      <ul className={typeToggle ? "typeDropDown" : "hidden typeDropDown"}>
+        {typeItems.map(typeItem => (
+          <li className="typeItem" key={typeItem}>
+            {typeItem}
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default Type;
+export default Type
