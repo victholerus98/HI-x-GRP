1ae9dba20e04814a01eb95d74d7f248f1a8ce446
LPlox <lina.js.wang@gmail.com>
2020-04-30 09:47:30 +0200

    Think the function works

--- src/components/TagsFilter.js
+++ src/components/TagsFilter.js
@@ -8,28 +8,44 @@ const TagsFilter = ({ data }) => {
   const setTags = (e) => {
     const tagValue = e.currentTarget.dataset.tag;
     if (activeTags.indexOf(tagValue) === -1) {
-      const newAddedArray = [...activeTags, tagValue];
-      setActiveTags(newAddedArray);
+      // const newAddedArray = [...activeTags, tagValue];
+      setActiveTags((prevArr) => [...prevArr, tagValue]);
       // console.log(activeTags);
     }
   };
 
   const removeTags = (e) => {
     const tagValue = e.currentTarget.dataset.tag;
-    const newRemovedArray = [...activeTags.filter((tag) => tag !== tagValue)];
-    setActiveTags(newRemovedArray);
+    // const newRemovedArray = [...activeTags.filter((tag) => tag !== tagValue)];
+    setActiveTags([...activeTags.filter((tag) => tag !== tagValue)]);
   };
 
   useEffect(() => {
     const newDataSet = data.filter((item) => {
+      let strictFilter = false;
       for (let i = 0; i < activeTags.length; i++) {
         const keyValue = activeTags[i];
-        return item[keyValue];
+        if (item[keyValue]) {
+          strictFilter = true;
+          console.log(i, activeTags[i], item[keyValue]);
+        } else {
+          strictFilter = false;
+        }
       }
+      return strictFilter;
     });
 
-    if (newDataSet.length === 0) setCardData(data);
-    if (newDataSet.length !== 0) setCardData(newDataSet);
+    // if (newDataSet.length === 0) setCardData(data);
+    // if (newDataSet.length !== 0) setCardData(newDataSet);
+
+    switch (newDataSet.length) {
+      case 0:
+        setCardData(data);
+        break;
+      default:
+        setCardData(newDataSet);
+        break;
+    }
   }, [activeTags, data]);
 
   return (
