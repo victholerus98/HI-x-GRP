5a973c8030e3b3f1e7c68b82d67586d10a509ff8
LPlox <lina.js.wang@gmail.com>
2020-05-05 20:45:33 +0200

    starting to see the animation movements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/card.scss";
 import Tags from "./Tags";
 
 const Card = ({ item }) => {
-  const { NameOfInitiative, Summary, Webaddress, logo } = item;
+  const [extraCard, setExtraCard] = useState(false);
+
+  const expandCard = () => {
+    setExtraCard(!extraCard);
+    console.log(extraCard);
+  };
+
+  const {
+    NameOfInitiative,
+    Summary,
+    Webaddress,
+    ActiveOnCovid,
+    DescriptionOnCOVID,
+    HyperlinkToCOVID,
+    logo,
+  } = item;
 
   return (
-    <div className="card">
-      <h2>{NameOfInitiative}</h2>
-      {logo ? <img src={`${logo}`} alt="logo" /> : null}
+    <div className="cards" onClick={expandCard}>
+      <div className="card">
+        <h2>{NameOfInitiative}</h2>
+        {logo ? <img src={`${logo}`} alt="logo" /> : null}
 
-      <p>{Summary}</p>
-      <div className="card-links">
-        <Tags item={item} />
-        {Webaddress ? (
-          <a className="homebutton" href={Webaddress}>
-            <object
-              data={require("../assets/homebtn.svg")}
-              aria-label="homebutton"
-            ></object>
-          </a>
-        ) : null}
+        <p>{Summary}</p>
+        <div className="card-links">
+          <Tags item={item} />
+          {Webaddress ? (
+            <a className="homebutton" href={Webaddress}>
+              <object
+                data={require("../assets/homebtn.svg")}
+                aria-label="homebutton"
+              ></object>
+            </a>
+          ) : null}
+        </div>
       </div>
+      {ActiveOnCovid ? (
+        <div className={extraCard ? `extraCard flipCard` : `extraCard`}>
+          <div className="cardBottom">
+            <p>COVID-19</p>
+            <a className="covid-btn" href={HyperlinkToCOVID}>
+              <object
+                data={require("../assets/externalLink-red.svg")}
+                aria-label="covid-address"
+              ></object>
+            </a>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
