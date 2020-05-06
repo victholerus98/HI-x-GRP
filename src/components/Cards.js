03e98ad706d8837eb5e3da2dbc8646b3f283ffd7
Joakim Hellgren <joakimhellgren@MacBook-Pro.local>
2020-05-06 13:24:12 +0200

    cards.js: added status if user search returns 0

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,17 +4,28 @@ import Masonry from "react-masonry-css";
 import "./styles/cards.scss";
 
 const Cards = ({ data }) => {
-  return (
-    <Masonry
-      breakpointCols={4}
-      className="my-masonry-grid"
-      columnClassName="my-masonry-grid_column"
-    >
-      {data.map((item, i) => {
-        return <Card key={i} item={item} />;
-      })}
-    </Masonry>
-  );
+  if (data.length !== 0) {
+    return (
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {data.map((item, i) => {
+          return <Card key={i} item={item} />;
+        })}
+      </Masonry>
+    );
+  } else {
+    return (
+      <Masonry
+        breakpointCols={1}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+      <p>There are no results matching your search</p>
+      </Masonry>
+    )}
 };
 
 export default Cards;
